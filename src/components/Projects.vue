a9c9432e0898b0ddf745d685c9660f57332f71b0
Nubaid Rashid <nubaidrashid369@gmail.com>
2025-04-03 18:19:03 +0600

    8th commit

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -12,7 +12,7 @@
         </p>
       </div>
 
-      <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
+      <div class="grid md:grid-cols-2 lg:grid-cols-2 gap-10">
         <!-- Project Card -->
         <div
           v-for="(project, index) in projects"
