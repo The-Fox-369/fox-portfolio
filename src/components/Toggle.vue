9cd2d1221804e3459f9572e1f321115d45000d73
Nubaid Rashid <nubaidrashid369@gmail.com>
2025-04-06 18:05:19 +0600

    28th commit

--- src/components/Toggle.vue
+++ src/components/Toggle.vue
@@ -7,7 +7,7 @@
       @change="toggleDark"
     />
     <div
-      class="relative h-6 w-11 rounded-full bg-[#302f2f] shadow-lg peer-checked:bg-[#e4e4e4] peer-focus:ring-4 peer-focus:outline-none after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-checked:after:border-white dark:border-[#1b1b1b] dark:peer-checked:bg-[#080808]"
+      class="relative h-6 w-11 rounded-full bg-[#929191] shadow-lg peer-checked:bg-[#e4e4e4] peer-focus:ring-4 peer-focus:outline-none after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-checked:after:border-white dark:border-[#1b1b1b] dark:bg-[#222121] dark:peer-checked:bg-[#080808]"
     ></div>
   </label>
 </template>
