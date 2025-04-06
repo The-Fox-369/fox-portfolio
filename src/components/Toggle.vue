96afb220423f1de10542197737bdfda4d9c5a024
Nubaid Rashid <nubaidrashid369@gmail.com>
2025-04-06 15:05:07 +0600

    18th commit

--- src/components/Toggle.vue
+++ src/components/Toggle.vue
@@ -7,12 +7,8 @@
       @change="toggleDark"
     />
     <div
-      class="w-11 h-6 bg-gray-300 peer-focus:outline-none dark:bg-gray-600 rounded-full peer peer-checked:bg-blue-600 transition-all duration-300"
-    >
-      <div
-        class="dot w-5 h-5 bg-white rounded-full shadow transform transition-transform duration-300 translate-x-1 peer-checked:translate-x-6"
-      ></div>
-    </div>
+      class="relative w-11 h-6 bg-[#e4e4e4] peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-[#1b1b1b] rounded-full peer dark:bg-[#1b1b1b] peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-[#1b1b1b] peer-checked:bg-[#e4e4e4] dark:peer-checked:bg-[#1b1b1b]"
+    ></div>
   </label>
 </template>
 
